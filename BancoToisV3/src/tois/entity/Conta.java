1090d4170aee9dd21689425d144580520656fafd
Matheus Ancelmo <Matheus Ancelmo@DESKTOP-RR7IH9N.Home>
2017-06-04 21:52:29 -0300

    ta saindo da jaula o monstro

--- BancoToisV3/src/tois/entity/Conta.java
+++ BancoToisV3/src/tois/entity/Conta.java
@@ -1,5 +1,91 @@
 package tois.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+@Entity
+@Table(name="conta")
 public class Conta {
+	
+	private String agencia = "1507-96";
+	private int conta = 0;
+	private String nome = "";
+	private String CPF = "";
+	private String RG = "";
+	
+	@NotEmpty(message="Selecione uma opção")
+	private String emprego = "";
+	
+	@NotEmpty(message="Selecione uma opção")
+	private String tipoConta = "";
+	
+	@NotEmpty(message="Escolha uma senha")
+	private String senha = "";
+
+	public String getAgencia() {
+		return agencia;
+	}
+
+	public void setAgencia(String agencia) {
+		this.agencia = agencia;
+	}
+
+	public int getConta() {
+		return conta;
+	}
+
+	public void setConta(int conta) {
+		this.conta = conta;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCPF() {
+		return CPF;
+	}
+
+	public void setCPF(String cPF) {
+		CPF = cPF;
+	}
+
+	public String getRG() {
+		return RG;
+	}
+
+	public void setRG(String rG) {
+		RG = rG;
+	}
+
+	public String getEmprego() {
+		return emprego;
+	}
+
+	public void setEmprego(String emprego) {
+		this.emprego = emprego;
+	}
+
+	public String getTipoConta() {
+		return tipoConta;
+	}
+
+	public void setTipoConta(String tipoConta) {
+		this.tipoConta = tipoConta;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
 
 }
--- BancoToisV3/src/tois/entity/Transacao.java
+++ BancoToisV3/src/tois/entity/Transacao.java
@@ -1,5 +1,64 @@
 package tois.entity;
 
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.Min;
+
+import tois.entity.Conta;
+
+@Entity
+@Table(name="transacao")
 public class Transacao {
+	
+	@Id
+	@GeneratedValue
+	private String id;
+	private String tipo;
+	@Min(1)
+	private double valor;
+	private Conta contaOrigem;
+	private String contaDestino;
+	
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getTipo() {
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	public double getValor() {
+		return valor;
+	}
+	public void setValor(double valor) {
+		this.valor = valor;
+	}
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "CONTA_ID")
+	public Conta getContaOrigem() {
+		return contaOrigem;
+	}
+	public void setContaOrigem(Conta contaOrigem) {
+		this.contaOrigem = contaOrigem;
+	}
+	public String getContaDestino() {
+		return contaDestino;
+	}
+	public void setContaDestino(String contaDestino) {
+		this.contaDestino = contaDestino;
+	}
 
 }
